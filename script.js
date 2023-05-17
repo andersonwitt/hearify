df471c747862f5078a03e04589dc436c97dd2d47
Anderson Witt <anderson.witt@worktrucksolutions.com>
2023-05-17 09:34:50 -0300

    updates

--- script.js
+++ script.js
@@ -21,120 +21,126 @@ let velocity = 1;
 let volume = 0.5;
 
 function repeat() {
-  if (!audio.paused) {
-    audio.pause();
-  }
-  audio.currentTime = 0;
+    if (!audio.paused) {
+        audio.pause();
+    }
+    audio.currentTime = 0;
 
-  audio.play();
+    audio.play();
 }
 
 function updateProgress() {
-  const currentTime = audio.currentTime;
-  const duration = audio.duration;
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
 
-  const progressPercent = (currentTime / duration) * 100;
-  progressRange.value = progressPercent;
+    const progressPercent = (currentTime / duration) * 100;
+    progressRange.value = progressPercent;
 
-  const currentMinutes = Math.floor(currentTime / 60);
-  const currentSeconds = Math.floor(currentTime % 60);
-  const durationMinutes = Math.floor(duration / 60);
-  const durationSeconds = Math.floor(duration % 60);
+    const currentMinutes = Math.floor(currentTime / 60);
+    const currentSeconds = Math.floor(currentTime % 60);
+    const durationMinutes = Math.floor(duration / 60);
+    const durationSeconds = Math.floor(duration % 60);
 
-  durationDisplay.textContent = `${formatTime(durationMinutes)}:${formatTime(
-    durationSeconds
-  )}`;
+    durationDisplay.textContent = `${formatTime(durationMinutes)}:${formatTime(
+        durationSeconds
+    )}`;
 
-  durationProgress.textContent = `${formatTime(currentMinutes)}:${formatTime(
-    currentSeconds
-  )}`;
+    durationProgress.textContent = `${formatTime(currentMinutes)}:${formatTime(
+        currentSeconds
+    )}`;
 
-  if (progressPercent == 100 && isRepeat) {
-    repeat();
-  }
+    if (progressPercent == 100) {
+        if (isRepeat) {
+            repeat();
+            return;
+        }
+
+        audio.currentTime = 0;
+        playButtonImage.src = "./assets/icons/play-icon.svg";
+    }
 }
 
 function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
+    return time < 10 ? `0${time}` : time;
 }
 
 audio.addEventListener("timeupdate", updateProgress);
 
 playButton.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.play();
-    audio.volume = volume;
-    playButtonImage.src = "./assets/icons/pause-icon.svg";
-    return;
-  }
+    if (audio.paused) {
+        audio.play();
+        audio.volume = volume;
+        playButtonImage.src = "./assets/icons/pause-icon.svg";
+        return;
+    }
 
-  audio.pause();
-  playButtonImage.src = "./assets/icons/play-icon.svg";
+    audio.pause();
+    playButtonImage.src = "./assets/icons/play-icon.svg";
 });
 
 progressRange.addEventListener("change", () => {
-  const progress = progressRange.value;
-  const duration = audio.duration;
-  audio.currentTime = (progress / 100) * duration;
+    const progress = progressRange.value;
+    const duration = audio.duration;
+    audio.currentTime = (progress / 100) * duration;
 });
 
 replayButton.addEventListener("click", repeat);
 
 repeatButton.addEventListener("click", () => {
-  isRepeat = !isRepeat;
-  if (isRepeat) {
-    repeatImage.src = "./assets/icons/repeat-selected-icon.svg";
-    return;
-  }
-  repeatImage.src = "./assets/icons/repeat-icon.svg";
+    isRepeat = !isRepeat;
+    if (isRepeat) {
+        repeatImage.src = "./assets/icons/repeat-selected-icon.svg";
+        return;
+    }
+    repeatImage.src = "./assets/icons/repeat-icon.svg";
 });
 
 fastForwardButton.addEventListener("click", () => {
-  if (velocity <= 2) {
-    velocity += 0.25;
+    if (velocity <= 2) {
+        velocity += 0.25;
 
-    audio.playbackRate = velocity;
-  }
+        audio.playbackRate = velocity;
+    }
 });
 
 fastRewindButton.addEventListener("click", () => {
-  if (velocity > 0.25) {
-    velocity -= 0.25;
+    if (velocity > 0.25) {
+        velocity -= 0.25;
 
-    audio.playbackRate = velocity;
-  }
+        audio.playbackRate = velocity;
+    }
 });
 
 volumeRange.addEventListener("input", (e) => {
-  volume = e.target.value;
-  audio.volume = volume;
-  if (volume == 0) {
-    volumeImage.src = "./assets/icons/volume-off-icon.svg";
-    return;
-  }
-  volumeImage.src = "./assets/icons/volume-icon.svg";
+    volume = e.target.value;
+    audio.volume = volume;
+    if (volume == 0) {
+        volumeImage.src = "./assets/icons/volume-off-icon.svg";
+        return;
+    }
+    volumeImage.src = "./assets/icons/volume-icon.svg";
 });
 
 volumeButton.addEventListener("click", () => {
-  if (audio.volume > 0) {
-    volumeRange.value = 0;
-    audio.volume = 0;
-    volumeImage.src = "./assets/icons/volume-off-icon.svg";
-    return;
-  }
-
-  volumeRange.value = volume;
-  audio.volume = volume;
-  volumeImage.src = "./assets/icons/volume-icon.svg";
-});
-
-favoriteButton.addEventListener('click', () => {
-    isFavorite = !isFavorite;
-
-    if(isFavorite) {
-        favoriteImage.src = './assets/icons/favorite-selected-icon.svg';
+    if (audio.volume > 0) {
+        volumeRange.value = 0;
+        audio.volume = 0;
+        volumeImage.src = "./assets/icons/volume-off-icon.svg";
         return;
     }
-    
-    favoriteImage.src = './assets/icons/favorite-icon.svg';
-})
+
+    volumeRange.value = volume;
+    audio.volume = volume;
+    volumeImage.src = "./assets/icons/volume-icon.svg";
+});
+
+favoriteButton.addEventListener("click", () => {
+    isFavorite = !isFavorite;
+
+    if (isFavorite) {
+        favoriteImage.src = "./assets/icons/favorite-selected-icon.svg";
+        return;
+    }
+
+    favoriteImage.src = "./assets/icons/favorite-icon.svg";
+});
